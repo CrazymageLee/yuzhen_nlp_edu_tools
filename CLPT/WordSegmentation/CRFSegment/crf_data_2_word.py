0f78418989c46cf9296bb99c6a73560d7589fd9f
Li <stormier@126.com>
2015-06-12 13:21:31 +0800

    make the text the same as before transformed

--- CLPT/WordSegmentation/CRFSegment/crf_data_2_word.py
+++ CLPT/WordSegmentation/CRFSegment/crf_data_2_word.py
@@ -8,24 +8,39 @@
 import codecs
 import sys
 
+
+
 def character_2_word(input_file, output_file):
     input_data = codecs.open(input_file, 'r', 'utf-8')
     output_data = codecs.open(output_file, 'w', 'utf-8')
+    is_first = True
     for line in input_data.readlines():
         if line == "\n":
-            output_data.write("\n")
+            if is_first == True:
+                output_data.write("\r\n")
+            else:
+                output_data.write(" \r\n")
+            is_first = True
         else:
             char_tag_pair = line.strip().split('\t')
             char = char_tag_pair[0]
-            tag = char_tag_pair[2]
+            tag = char_tag_pair[1]
             if tag == 'B':
-                output_data.write(' ' + char)
+                if is_first == True:
+                    output_data.write(char)
+                    is_first = False
+                else:
+                    output_data.write(' ' + char)
             elif tag == 'M':
                 output_data.write(char)
             elif tag == 'E':
                 output_data.write(char + ' ')
             else: # tag == 'S'
-                output_data.write(' ' + char + ' ')
+                if is_first == True:
+                    output_data.write(char + ' ')
+                    is_first = False
+                else:
+                    output_data.write(' ' + char + ' ')
     input_data.close()
     output_data.close()
 
